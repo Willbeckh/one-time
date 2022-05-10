ef4d02c6b58c94738148cf2837c92bf4ec065f8d
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-10 14:56:25 +0300

    ft: create application routes

--- app/main/views/routes.py
+++ app/main/views/routes.py
@@ -1,13 +1,17 @@
+from app.models import Pitch
 from .. import main
 from flask import render_template, redirect, flash, url_for
+from flask_login import login_required
 
 
 @main.route('/')
 @main.route('/index')
+@login_required
 def index():
     """
     Function for rendering the root page.
     """
-    return render_template('index.html')
-
-
+    pitches = Pitch.query.all()
+    print(pitches)
+    title = 'Pitchy'
+    return render_template('index.html', title=title, pitches=pitches)
