880375be61d03878b44d1eb8988688de2f1281f8
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-10 20:31:38 +0300

    ft: create user login function

--- app/main/auth/login.py
+++ app/main/auth/login.py
@@ -3,7 +3,8 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_user, logout_user
 from app.models import User
 from . import auth
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
+from app import db
 
 # login route
 @auth.route('/login', methods=['GET', 'POST'])
@@ -30,3 +31,23 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+
+# register route 
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    '''
+    user sign up route method
+    '''
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account for {user.username} successfully registerd!')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', title='Sign Up', form=form)
+        
