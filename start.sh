51c207c74342b515fa35c1694c618eecad2c7adf
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-10 15:17:07 +0300

    ch: add app runner instance

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://billy:pass12@localhost/pit
 export FLASK_APP=run.py
 export FLASK_ENV=development
 export SECRET_KEY="d\x12S\xbe\xd1\x04\x9d\xf7\xc3\xf0@\xbcM|\x"
+
 # flask run
-python3 manage.py shell
-# python3 manage.py server
+# python3 manage.py shell
+python3 manage.py server
