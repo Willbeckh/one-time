315176c1f1b834db45c94e976dee2cc1077a6e05
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-10 14:39:54 +0300

    ch: create application factory

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import config_options
 
 db = SQLAlchemy()  # db instance
-
+login_manager = LoginManager()
+login_manager.login_view  = 'auth.login'
 
 def create_app(config_name):
     # flask app instance
     app = Flask(__name__)
     Bootstrap(app)
 
+    login_manager.init_app(app)
+    
     # register blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -22,7 +26,7 @@ def create_app(config_name):
 
     # app configs
     app.config.from_object(config_options[config_name])
-    
+
     db.init_app(app)
     migrate = Migrate(app, db)
 
