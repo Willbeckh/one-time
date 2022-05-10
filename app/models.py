7013fd2598942bc186a7830140717e807b7ef4ae
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-10 14:57:45 +0300

    create database models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,16 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from . import db
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(id):
+    '''method that keeps the id of the current authenticated user'''
+    return User.query.get(int(id))
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
@@ -14,6 +22,19 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.email}> - <Bio: {self.about_me}>'
+
+    # @property
+    # def password(self):
+    #     raise AttributeError('Password not accessble!')
+
+    # @password.setter
+    def set_password(self, password):
+        """ Method to create a hashed password."""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        '''Verifies if a password is hashed.'''
+        return check_password_hash(self.password_hash, password)
 
 
 class Pitch(db.Model):
