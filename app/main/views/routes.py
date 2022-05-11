73aaf02c684ba2b1ff8fa53bb32b24aa22adc8af
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-12 01:25:15 +0300

    ft: create dynamic route for adding a new pitch

--- app/main/views/routes.py
+++ app/main/views/routes.py
@@ -1,9 +1,10 @@
+from crypt import methods
 from flask import render_template, redirect, flash, url_for, request
 from flask_login import login_required, current_user
 from .. import main  # blueprint
 from app import db
 from app.models import User, Pitch  # db models
-from app.forms import ProfileUpdate
+from app.forms import ProfileUpdate, AddPost
 
 # TODO: create a before request to get user last seen time.
 
@@ -60,3 +61,18 @@ def update_profile():
 #         user.display_pic_path = path
 #         db.session.commit()
 #     return redirect(url_for('main.user', username=current_user))
+
+# crete form for creating a new post
+@main.route('/add_post', methods=['GET', 'POST'])
+@login_required
+def add_post():
+    """Method to enable a user to create a new post"""
+    form =  AddPost()
+    # validate form on submit
+    if form.validate_on_submit():
+        pitch = Pitch(title=form.title.data, text=form.text.data, author=current_user)
+        db.session.add(pitch)
+        db.session.commit()
+        flash(f"Post: '{pitch.title}' created successfully")
+        return redirect(url_for('main.index'))
+    return render_template('new_pitch.html', title="Add post", form=form)
