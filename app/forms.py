9a8b0c85c80803694a714076af10b8a52a7de5dd
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-11 14:21:18 +0300

    Revert "ft; create user profile update"
    This reverts commit 4a515a06e1e75cea23d0279e97b58568161f18b7.

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired
 from wtforms import ValidationError
-from wtforms.validators import Email, EqualTo
+from wtforms.validators import Email,EqualTo
 from .models import User
-
 
 class LoginForm(FlaskForm):
     """
@@ -24,8 +23,7 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[
-                              DataRequired(), EqualTo('password')])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_user(self, username):
@@ -37,11 +35,3 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             return ValidationError("there is an account with that Email!")
-
-
-# user profile update form
-class ProfileUpdate(FlaskForm):
-    """ form for updating a user profile"""
-    username = StringField('Username', validators=[DataRequired()])
-    about_me = StringField('Bio', validators=[Length(min=20, max=200)])
-    submit = SubmitField('Update')
