58cf1747f429b7d84e0f385c1688c4c403fa3c2a
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-11 15:53:37 +0300

    ft: create flask route for editting a user profile

--- app/main/views/routes.py
+++ app/main/views/routes.py
@@ -1,8 +1,11 @@
-from app.models import Pitch
-from .. import main
-from flask import render_template, redirect, flash, url_for
-from flask_login import login_required
-from app.models import User, Pitch
+from flask import render_template, redirect, flash, url_for, request
+from flask_login import login_required, current_user
+from .. import main  # blueprint
+from app import db
+from app.models import User, Pitch  # db models
+from app.forms import ProfileUpdate
+
+# TODO: create a before request to get user last seen time.
 
 
 @main.route('/')
@@ -13,8 +16,7 @@ def index():
     Function for rendering the root page.
     """
     pitches = Pitch.query.all()
-    title = 'Pitchy'
-    return render_template('index.html', title=title, pitches=pitches)
+    return render_template('index.html', title='Pitchy', pitches=pitches)
 
 
 # view availabe posts for specific user
@@ -26,3 +28,22 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     pitches = Pitch.query.all()
     return render_template('user.html', user=user, pitches=pitches)
+
+
+# user update route
+@main.route('/update_profile', methods=['GET', 'POST'])
+@login_required
+def update_profile():
+    """method to update a user profile"""
+    # create form obj
+    form = ProfileUpdate()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Profile successfully updated.')
+        return redirect(url_for('main.user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.username.data = current_user.username
+    return render_template('edit_profile.html', title='Update', form=form)
