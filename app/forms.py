36a2f3a57cef8f89f9556a2bae3b5b490cd18a75
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-12 01:23:18 +0300

    ft: create form for creating a new pitch

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 from wtforms import ValidationError
 from wtforms.validators import Email, EqualTo
@@ -42,5 +42,13 @@ class RegisterForm(FlaskForm):
 class ProfileUpdate(FlaskForm):
     """class for creating a wtf form for updating user profile"""
     username = StringField('Username', validators=[DataRequired()])
-    about_me = StringField('Bio', validators=[Length(min=2, max=200)])
+    about_me = TextAreaField('Bio', validators=[Length(min=2, max=200)])
     submit = SubmitField('Update')
+    
+
+class AddPost(FlaskForm):
+    """class that defines the add post form"""
+    # title, text, submit
+    title = StringField('Title', validators=[DataRequired()])
+    text = TextAreaField('Text: ', validators=[Length(min=20, max=255)])
+    submit = SubmitField('Submit')
