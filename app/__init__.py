d21832954e27674870bbb43bde0781a0be644622
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-11 23:31:49 +0300

    create email service entry point

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_login import LoginManager
+# from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 from config import config_options
 
 db = SQLAlchemy()  # db instance
 login_manager = LoginManager()
-login_manager.login_view  = 'auth.login'
+login_manager.login_view = 'auth.login'
+# photos = UploadSet('photos', IMAGES)
+mail = Mail()
+
 
 def create_app(config_name):
     # flask app instance
@@ -15,7 +20,8 @@ def create_app(config_name):
     Bootstrap(app)
 
     login_manager.init_app(app)
-    
+    mail.init_app(app)
+
     # register blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -24,12 +30,13 @@ def create_app(config_name):
     from .main.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
+    # configure uploadset
+    # configure_uploads(app, photos)
+
     # app configs
     app.config.from_object(config_options[config_name])
 
     db.init_app(app)
     migrate = Migrate(app, db)
-
-    # init flask extensions
 
     return app
