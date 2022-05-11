9cd15ac7cbc60f6a705e23e8ba2386e598971610
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-11 12:46:14 +0300

    add route that returns a specific user.

--- app/main/views/routes.py
+++ app/main/views/routes.py
@@ -2,6 +2,7 @@ from app.models import Pitch
 from .. import main
 from flask import render_template, redirect, flash, url_for
 from flask_login import login_required
+from app.models import User, Pitch
 
 
 @main.route('/')
@@ -14,3 +15,14 @@ def index():
     pitches = Pitch.query.all()
     title = 'Pitchy'
     return render_template('index.html', title=title, pitches=pitches)
+
+
+# view availabe posts for specific user
+@main.route('/user/<username>')
+def user(username):
+    """
+    method to query db for a specific user
+    """
+    user = User.query.filter_by(username=username).first_or_404()
+    pitches = Pitch.query.all()
+    return render_template('user.html', user=user, pitches=pitches)
