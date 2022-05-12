922fef0571dbfbf5d544d407c09b2385d709ff32
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-12 14:25:12 +0300

    ft: post-- create route for viewing single post & view rout for commenting a post

--- app/main/views/routes.py
+++ app/main/views/routes.py
@@ -1,10 +1,10 @@
-from crypt import methods
+from cmath import pi
 from flask import render_template, redirect, flash, url_for, request
 from flask_login import login_required, current_user
 from .. import main  # blueprint
 from app import db
-from app.models import User, Pitch  # db models
-from app.forms import ProfileUpdate, AddPost
+from app.models import User, Pitch, Comment, Vote  # db models
+from app.forms import ProfileUpdate, AddPost, CommentForm
 
 # TODO: create a before request to get user last seen time.
 
@@ -59,9 +59,40 @@ def add_post():
     # validate form on submit
     if form.validate_on_submit():
         pitch = Pitch(title=form.title.data,
-                      text=form.text.data, author=current_user)
+                      text=form.text.data, author=current_user, categories=form.categories.data)
         db.session.add(pitch)
         db.session.commit()
         flash(f"Post: '{pitch.title}' created successfully")
         return redirect(url_for('main.index'))
     return render_template('new_pitch.html', title="Add post", form=form)
+
+
+# view single pitch view
+@main.route('/post/<int:pitch_id>')
+# @login_required
+def post(pitch_id):
+    pitch = Pitch.query.get_or_404(pitch_id)
+    comments = Comment.query.all()
+    return render_template('post.html', title=pitch.title, pitch=pitch,  comments=comments)
+
+
+# create a comment route
+@main.route('/post/<int:pitch_id>/comment', methods=['GET', 'POST'])
+@login_required
+def comment(pitch_id):
+    """Method to enable a user to comment on a post"""
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(comment_text=form.comment_text.data,
+                          author=current_user, pitch_id=pitch_id)
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('main.post', pitch_id=pitch_id))
+    return render_template('comment.html', title="Comment", form=form)
+
+
+# TODO: create a route for voting on a post
+# vote on a post
+# @main.route('/post/<int:pitch_id>/vote', methods=['GET', 'POST'])
+# def vote(pitch_id):
+    
